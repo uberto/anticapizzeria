e33e69d2c5ba77352536ba24d911180c08249f60
Uberto Barbini <uberto.gama@gmail.com>
2018-04-17 20:49:40 +0100

    added main

--- src/main/java/com/gamasoft/anticapizzeria/Main.kt
+++
@@ -1,4 +0,0 @@
-package com.gamasoft.anticapizzeria
-
-class Main {
-}
---
+++ src/main/kotlin/com/gamasoft/anticapizzeria/Main.kt
@@ -0,0 +1,6 @@
+package com.gamasoft.anticapizzeria
+
+fun main(args : Array<String>)  {
+        println("Antica Pizzeria! Best Pizza outside Naples")
+
+}
